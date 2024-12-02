b21d778fd602319fa2a100dba1215875b0af57d8
Asiya-Naser <khalidmukemil@gmail.com>
2024-12-02 12:25:15 +0300

    deploy

--- Amazon-project-I/vite.config.js
+++ Amazon-project-I/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base:"/Amazon-part-one-project/",
+  base:"/Amazon-project-I/",
   plugins: [react()],
-})
+});
